1dca0484cddfea18b28270cd71ca9037a0bbd825
ahmedamamff-commits <ahmedamamff@gmail.com>
2026-09-13 13:17:50 -0700

    Add files via upload

---
+++ Homework16.py
@@ -0,0 +1,5 @@
+Age = int(input("Please enter your age:"))
+if Age != int(Age):
+    print("Sorry, that is not a valid age. Please enter a valid one.")
+else:
+    print("Thanks! You have entered a valid age of", Age)
